da4235acc0689c553a89ff1dd3a65623080fb039
conao3 <conao3@gmail.com>
2025-09-07 13:56:30 +0900

    rename

--- src/20250907-clojure-mcp/sample/aws-knowledge-mcp/cdk/bin/cdk-lambda-hello.ts
+++ src/20250907-clojure-mcp/sample/aws-knowledge-mcp/cdk/bin/cdk-lambda-hello.ts
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
-import { CdkLambdaHelloStack } from "../lib/cdk-lambda-hello-stack";
+import { Stack } from "../lib/stack";
 
 const app = new cdk.App();
 
 const environment = "dev";
 const project = "mcptest";
 
-new CdkLambdaHelloStack(app, `${environment}-${project}-stack`, {
+new Stack(app, `${environment}-${project}-stack`, {
   environment,
   project,
 });
--- src/20250907-clojure-mcp/sample/aws-knowledge-mcp/cdk/lib/stack.ts
+++ src/20250907-clojure-mcp/sample/aws-knowledge-mcp/cdk/lib/stack.ts
@@ -2,13 +2,13 @@ import * as cdk from "aws-cdk-lib";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import { Construct } from "constructs";
 
-export interface CdkLambdaHelloStackProps extends cdk.StackProps {
+export interface StackProps extends cdk.StackProps {
   environment: string;
   project: string;
 }
 
-export class CdkLambdaHelloStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props: CdkLambdaHelloStackProps) {
+export class Stack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
 
     const prefix = `${props.environment}-${props.project}`;
